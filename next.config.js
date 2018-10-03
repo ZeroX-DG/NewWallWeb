29b67d9d8898bd8d7707b76f9124acbdf8053b3a
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-10-03 23:34:12 +0700

    added assert prefix

--- next.config.js
+++ next.config.js
@@ -1,2 +1,4 @@
 const withImages = require("next-images");
-module.exports = withImages();
+module.exports = withImages({
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/NewWallWeb' : ''
+});
