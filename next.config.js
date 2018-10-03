218172cf4f3362f7f01ea1043613c870e023d671
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-10-03 23:35:37 +0700

    updated config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 const withImages = require("next-images");
 module.exports = withImages({
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/NewWallWeb' : ''
+  assetPrefix: process.env.NODE_ENV === "production" ? "/NewWallWeb" : ""
 });
